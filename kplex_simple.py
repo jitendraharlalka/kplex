d138d5511699cb2913df48d9c1e9343c0ecc3899
harenberg <sdharenb@ncsu.edu>
2013-06-27 19:44:32 -0400

    code

--- kplex_simple.py
+++ kplex_simple.py
@@ -1,5 +1,7 @@
 import sys
+import itertools
 
+#creat graph adj list from file
 def load_graph(file_name):
     graph_file = open(file_name, 'r')
     num_vertices = 0
@@ -13,8 +15,52 @@ def load_graph(file_name):
         if v > num_vertices:
             num_vertices = v
         num_edges += 1
-
+    
     graph_file.close()
 
+    
+    graph = [set() for i in range(num_vertices+1)]  #+1 for 1 based indexing of vertices
+    for e in edgelist:
+        u,v = e
+        graph[u].add(v)
+        graph[v].add(u)
+    return graph
 
-load_graph(sys.argv[1])
+
+#find all 3-cliques in a graph
+def find_triangles(graph):
+    triangles = []
+    done_vertices = set()
+    for v in range(1,len(graph)):
+        Nv = list(graph[v]-done_vertices)
+        if len(Nv) < 2: #no triangle containing this vertex
+            continue
+        for pair in list(itertools.combinations(Nv,2)):
+            if pair[0] in graph[pair[1]]:
+                triangle = list(pair)+[v]
+                triangle.sort()
+                triangles.append(triangle)
+
+        done_vertices.add(v)
+    return triangles       
+
+
+#finds the best kplex for a triangle (3-clique)
+def get_kplex(graph, triangle):
+    print(triangle)
+    curr_core = set(triangle)
+    peripherals = set()
+    for v in curr_core:
+        peripherals |= graph[v]
+    peripherals -= curr_core
+    
+    while True:
+        break
+    
+    print(peripherals)
+
+
+graph = load_graph(sys.argv[1])
+triangles = find_triangles(graph)
+triangle = triangles[0]
+get_kplex(graph, triangle)
